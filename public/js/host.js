8d0fa359c906805c2c3e8dc8887ec7e32fe1fc74
Ethan <ethannbrimhall@gmail.com>
2018-06-15 13:58:57 -0400

    Added design to host screen

--- public/js/host.js
+++ public/js/host.js
@@ -3,8 +3,7 @@ var params = jQuery.deparam(window.location.search);
 
 //When host connects to server
 socket.on('connect', function() {
-    
-    document.getElementById('numPlayers').innerHTML = "Players: 0";
+
     document.getElementById('players').value = "";
     
     //Tell server that it is host connection
@@ -18,16 +17,12 @@ socket.on('showGamePin', function(data){
 //Adds player's name to screen and updates player count
 socket.on('updatePlayerLobby', function(data){
     
-    var numberOfPlayers = 0;
     document.getElementById('players').value = "";
     
     for(var i = 0; i < data.length; i++){
-        numberOfPlayers += 1;
         document.getElementById('players').value += data[i].name + "\n";
-        document.getElementById('numPlayers').innerHTML = numPlayers;
     }
     
-    document.getElementById('numPlayers').innerHTML = "Players: " + numberOfPlayers;
 });
 
 //Tell server to start game if button is clicked
